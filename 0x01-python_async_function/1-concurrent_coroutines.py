fcf96d655a3d26a7a7ba366ab700fd4c7c96a44a
Motunrayo <ajirotutu4real@gmail.com>
2024-03-18 17:44:12 +0100

    Added task 3

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Basic Async"""
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait N"""
     return sorted([await wait_random(max_delay) for _ in range(n)])
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Measure Runtime"""
-wait_n = __import__('1-concurrent_coroutines').wait_n
 import asyncio
 import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
