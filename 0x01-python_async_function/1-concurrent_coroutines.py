d01d50ca4ff47eb6900c5c0adbcd79177993d2a8
Motunrayo <ajirotutu4real@gmail.com>
2024-03-18 17:34:03 +0100

    Added task 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,6 @@
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """Wait N"""
-    return [await wait_random(max_delay) for _ in range(n)]
+    return sorted([await wait_random(max_delay) for _ in range(n)])
